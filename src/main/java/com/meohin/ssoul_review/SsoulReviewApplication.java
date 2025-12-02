15085a7010c79558f375bbeb984b82d1c81923a3
MEOHIN <meohin@gmail.com>
2025-12-02 17:49:23 +0900

    feat: 스케줄링, 캐싱 및 환경변수 로드 기능 추가

--- src/main/java/com/meohin/ssoul_review/SsoulReviewApplication.java
+++ src/main/java/com/meohin/ssoul_review/SsoulReviewApplication.java
@@ -1,12 +1,18 @@
 package com.meohin.ssoul_review;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableCaching
 public class SsoulReviewApplication {
 
 	public static void main(String[] args) {
+		Dotenv.load();
 		SpringApplication.run(SsoulReviewApplication.class, args);
 	}
 
