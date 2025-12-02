7083dd8fa17db2940de054e5179de772e12e3f93
MEOHIN <meohin@gmail.com>
2025-12-02 15:30:01 +0900

    Initial commit

---
+++ src/main/java/com/meohin/ssoul_review/SsoulReviewApplication.java
@@ -0,0 +1,13 @@
+package com.meohin.ssoul_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoulReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoulReviewApplication.class, args);
+	}
+
+}
