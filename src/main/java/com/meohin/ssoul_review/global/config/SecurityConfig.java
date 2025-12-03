1f0cdd9f20ad5f13fadc7c6c0a2e5c465d74f92b
MEOHIN <meohin@gmail.com>
2025-12-03 16:07:17 +0900

    feat: SecurityConfig 추가

---
+++ src/main/java/com/meohin/ssoul_review/global/config/SecurityConfig.java
@@ -0,0 +1,10 @@
+package com.meohin.ssoul_review.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
